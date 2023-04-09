62049892f4ba1b5b5bad53cf09db504dcb0f14b5
taivo2209 <taivo1213@gmail.com>
2023-04-09 12:32:49 +0700

    updated

---
+++ src/components/dashboard/my-categories/ModalForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { useForm } from "react-hook-form";
+
+const ModalForm = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Button color="danger" onClick={toggle}>
+        Click Me
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} {...args}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Do Something
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalForm;
